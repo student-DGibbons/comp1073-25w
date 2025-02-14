bbc9b85ebd457f9221e103b05554b3a60de61be8
student-DGibbons <200230810@student.georgianc.on.ca>
2025-02-14 14:47:47 -0500

    Finished events 2 javascript

--- lesson-6/js/events2.js
+++ lesson-6/js/events2.js
@@ -1,17 +1,22 @@
 // Declare and initialize variables
-const elem = document.querySelector('p');
+// const elem = document.querySelector('p:first-of-type');
+const elem = document.querySelector('h1');
+
 
 // Functions
 function random(number) {
     return Math.floor(Math.random()*(number + 1));
 }
 /* STEP 1a: Event Objects
-Modify the below function to allow it to accept the event as a parameter or argument (named "event" in this case), then change the background color of the event.target instead of the document body */
-function bgChange() {
+Modify the below function to allow it to accept the event as a parameter or 
+argument (named "event" in this case), then change the background color of 
+the event.target instead of the document body */
+function bgChange(event) {
     var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-    document.body.style.backgroundColor = rndCol;
+    event.target.style.backgroundColor = rndCol;
 }
-// The target property of the event is a reference to the element that the event was acted upon
+// The target property of the event is a reference to the element that the 
+// event was acted upon
 
 /* STEP 1b: Change the element that features the click event listener by modifying the global const declaration at the top of the script to target the <h1> instead */
 
@@ -27,6 +32,15 @@ const submit = document.getElementById('submit');
 const para = document.querySelector('#formErrors');
 
 /* STEP 2b: Create a script to capture the onsubmit event using preventDefault() */
+
+form.addEventListener("submit", function(event){
+    // console.log(event);
+    if(email.value === ""){
+        //Email field is empty, so don't let the form submit
+        event.preventDefault();
+        para.textContent = "You need to type in your email.";
+    }
+});
 
 // Next, open up gallery.html...
 
