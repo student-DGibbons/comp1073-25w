551d4ff04a15296cb0fe522c1460cd23332802f5
student-DGibbons <200230810@student.georgianc.on.ca>
2025-03-21 14:00:18 -0400

    Worked on json and fetch

--- lesson-10/js/json.js
+++ lesson-10/js/json.js
@@ -1,26 +1,28 @@
 /* STEP 2: Reference the HEADER and the SECTION elements with variables */
-
+const header = document.querySelector("header");
+const section = document.querySelector("section");
 
 // STEP 3a: Create the asynchronous function populate()
-
+async function populate(){
     // Introducing JavaScript Object Notation (JSON): https://json.org/
     // STEP 4: Store the URL of a JSON file in a variable */
-    
+    const requestURL = "https://student-dgibbons.github.io/comp1073-25w/lesson-10/js/i-scream.json";
     // STEP 5: Use the new URL to create a new request object
-    
+    const request = new Request(requestURL);
     // STEP 6: Make a network request with the fetch() function, which returns a Response object
-    
+    const response = await fetch(request);
     // STEP 7: Capture the returned Response object and covert to a JSON object using json()
-    
+    const iScream = await response.json();
     // STEP 8: Output the iScream JSON object to the console 
-    
+    console.log(iScream);
     // STEP 9a: Invoke the populateHeader function here, then build it below
     
     // STEP 10a: Invoke the showTopFlavors function here, then build it below
-    
 
+}
 
 // STEP 3b: Call the populate() function
+populate();
 
 
 /* STEP 9b: Build out the populateHeader() function */
