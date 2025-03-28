d436c4d16aabe7dbb1323bf3e93195e8bd6f1858
student-DGibbons <200230810@student.georgianc.on.ca>
2025-03-28 15:44:20 -0400

    Completed NY Times API Demo

--- lesson-11/js/nytimes.js
+++ lesson-11/js/nytimes.js
@@ -26,21 +26,27 @@ function fetchResults(event) {
     if(endDate.value !==""){
         url += `&end_date=${endDate.value.replaceAll("-", "")}`;
     };
+    console.log(url);
     // STEP 4: Use fetch() to pass the URL that we built as a request to the API service, then pass the JSON to the displayResults() function
-    console.log(url);    
-
+    fetch(url).then(result => {
+        return result.json();
+    }).then(json => {
+        displayResults(json);
+        // console.log(json);
+    });
 };
 
 function displayResults(json) {
     // STEP 5: Log to the console the results from the API
-    
+    // console.log(json);
 
     // Clear out the old results…
     while (section.firstChild) {
             section.removeChild(section.firstChild);
     };
     // STEP 6: Create the variable articles to capture the articles from the JSON object
-    	
+    let articles = json.response.docs;
+    console.log(articles);
 
     if (articles.length === 0) {
         const para = document.createElement('p');
@@ -55,20 +61,27 @@ function displayResults(json) {
             const para1 = document.createElement('p');
 
             const current = articles[i];
-            console.log(current);
+            // console.log(current);
             // STEP 7a: Look at the console output from the API…
-            
+            link.href = articles[i].web_url;
+            link.textContent = articles[i].headline.main;
+            // console.log(link);
             // STEP 7b: Grab the content from the JSON for the hyperlink and the article teaser (snippet)
-            
+            para1.textContent = articles[i].snippet;
+            // console.log(para1);
 
             if(current.multimedia.length > 0) {
                 img.src = 'https://www.nytimes.com/' + current.multimedia[0].url;
                 img.alt = current.headline.main;
             };
             // STEP 8: Put each article together as an ARTICLE element and append it as a child of the SECTION element in the HTML
-            
-
-            
+            article.appendChild(heading);
+            heading.appendChild(link);
+            article.appendChild(img);
+            article.appendChild(para1);
+            console.log(article);
+            // Inject completed article element into page
+            section.appendChild(article);
         };
     };
 };
